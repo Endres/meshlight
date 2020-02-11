3e881630c255422436a58e53110394143e6ffe46
Endres <git@e7p.de>
2020-02-11 01:36:11 +0100

    config update

--- server/config.py
+++ server/config.py
@@ -3,3 +3,8 @@
 TARGET_FPS = 30
 
 MESH_TIMEOUT = 9.5
+
+DEFAULT_CONFIGURATION = [
+    {"animation": "RGBFader", "duration": 300, "event": {"type": "Always"}},
+    {"animation": "BrightnessFader", "duration": 20, "event": {"type": "Always"}}
+]
