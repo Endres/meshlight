edab953c8990cdfab7276e10b58e16fe42e66cbd
pi <git@e7p.de>
2020-03-14 13:22:21 +0000

    exh config

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ MESH_TIMEOUT = 9.5
 DEFAULT_CONFIGURATION = [
     #{"animation": "Treppenblink", "duration": 30, "event": "Always", "until": "Always"},
     {"animation": "Blackout", "duration": 600, "event": "Always", "until": "Always"},
-    {"animation": "RGBFader", "duration": 20, "event": "GeoTimed(Sunset)", "until": "GeoTimed(Sunrise)"},
+    {"animation": "RGBFader", "duration": 300, "event": "GeoTimed(Sunset)", "until": "GeoTimed(Sunrise)"},
     {"animation": "BrightnessFader", "duration": 20, "event": "GeoTimed(Sunset)", "until": "GeoTimed(Sunrise)"}
 ]
 
